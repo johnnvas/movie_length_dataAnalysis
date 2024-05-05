c2cd3a368620957d7a709229bf0ae2a073cb9e38
Johnn <j.christian.vasquez@gmail.com>
2024-05-04 20:03:43 -0400

    edited

--- main.py
+++ main.py
@@ -9,4 +9,17 @@ netflix_subset = netflix_df[netflix_df['type'] == "Movie"]
 netflix_movies = netflix_subset[['title', 'country', 'genre', 'release_year', 'duration']]
 short_movies = netflix_movies[netflix_movies['duration'] < 60]
 
+
+colors=[]
+
+for index, movie in netflix_movies.iterrows():
+    if movie['genre'] == "Children":
+        colors.append('orange')
+    elif movie['genre'] == "Documentaries":
+        colors.append('red')
+    elif movie['genre'] == "Stand-Up":
+        colors.append('black')
+    else:
+        colors.append('blue')
+
 print(short_movies.head(50))
